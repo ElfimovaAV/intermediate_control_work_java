85397f6ec32cdab058d65a116e886cbca968f064
Anastasia <nastya.elfimova@inbox.ru>
2023-07-06 02:27:38 +0500

    Version 2 is complete

--- src/toy_store/Main.java
+++ src/toy_store/Main.java
@@ -10,16 +10,16 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        Toy toy1 = new Toy(0, "Doll", 30);
-        Toy toy2 = new Toy(1, "Car", 40);
-        Toy toy3 = new Toy(2, "Robot", 5);
-        Toy toy4 = new Toy(3, "Puppet", 70);
-        Toy toy5 = new Toy(4, "Doll", 30);
-        Toy toy6 = new Toy(5, "Car", 40);
-        Toy toy7 = new Toy(6, "Robot", 5);
-        Toy toy8 = new Toy(7, "Puppet", 70);
-        Toy toy9 = new Toy(8, "Doll", 30);
-        Toy toy10 = new Toy(9, "Car", 40);
+        Toy toy1 = new Toy(0, "Doll", 3,30);
+        Toy toy2 = new Toy(1, "Car", 2, 40);
+        Toy toy3 = new Toy(2, "Robot", 1, 5);
+        Toy toy4 = new Toy(3, "Puppet", 3, 70);
+        Toy toy5 = new Toy(4, "Teddy Bear", 2, 30);
+        Toy toy6 = new Toy(5, "Ball", 1,40);
+        Toy toy7 = new Toy(6, "Whirligig", 3, 5);
+        Toy toy8 = new Toy(7, "Blocks", 2, 70);
+        Toy toy9 = new Toy(8, "Lock blocks", 3, 30);
+        Toy toy10 = new Toy(9, "Train", 1, 40);
 
         List<Toy> toys = new ArrayList<Toy>();
         toys.add(toy10);
@@ -34,16 +34,23 @@ public class Main {
         toys.add(toy1);
 
         ToyStore toyStore = new ToyStore(toys);
-        toyStore.saveToyForLottery();
-        toyStore.saveToyForLottery();
-        toyStore.saveToyForLottery();
-        toyStore.saveToyForLottery();
-        toyStore.saveToyForLottery();
-        toyStore.saveToyForLottery();
-        toyStore.saveToyForLottery();
-        toyStore.saveToyForLottery();
-        toyStore.saveToyForLottery();
-        toyStore.saveToyForLottery();
+        toyStore.getPrizeToy();
+        toyStore.getPrizeToy();
+        toyStore.getPrizeToy();
+        toyStore.getPrizeToy();
+        toyStore.getPrizeToy();
+        toyStore.getPrizeToy();
+        toyStore.getPrizeToy();
+        toyStore.getPrizeToy();
+        toyStore.getPrizeToy();
+        toyStore.getPrizeToy();
+        toyStore.getPrizeToy();
+        toyStore.getPrizeToy();
+        toyStore.getPrizeToy();
+        toyStore.getPrizeToy();
+        toyStore.getPrizeToy();
+        toyStore.getPrizeToy();
+        toyStore.getPrizeToy();
 
 
     }
